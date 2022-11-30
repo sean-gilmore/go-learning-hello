8688424c06959fc23be3fab470674cc017fbee0f
Sean Gilmore <gilmore.seven@gmail.com>
2022-11-30 20:12:37 +1100

    Handle errors tutorial
    https://go.dev/doc/tutorial/handle-errors
    
    - Configure the log package to print the command name ("greetings: ") at the start of its log messages, without a time stamp or source file information.
    - Assign both of the Hello return values, including the error, to variables.
    - Change the Hello argument from Gladys’s name to an empty string, so you can try out your error-handling code.
    - Look for a non-nil error value. There's no sense continuing in this case.
    - Use the functions in the standard library's log package to output error information. If you get an error, you use the log package's Fatal function to print the error and stop the program.

--- hello.go
+++ hello.go
@@ -10,11 +10,17 @@ func main() {
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
 	// the time, source file and line number.
-	log.SetPrefix("greetings:")
+	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	
+
 	// Request a greeting message
 	message, err := greetings.Hello("")
-	
+
+	// If an error was returned, print it to the console and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error message was returned, print it to the console
 	fmt.Println(message)
 }
